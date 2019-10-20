864d5ec50bdcebea4cf471e9d78601c8f118de85
Evan Shaw <evandanielshaw@gmail.com>
2019-10-20 17:35:32 +0900

    feat: add Router and Dispatcher

---
+++ Router.php
@@ -0,0 +1,178 @@
+<?php
+namespace Aivec\WordPress\Routing;
+
+/**
+ * Request handler factory
+ *
+ * NOTE: This class MUST be instantiated AFTER WordPress core functions are loaded (ie. some
+ * time after 'plugins_loaded', 'init', or any other appropriate WordPress hook)
+ */
+class Router extends Dispatcher {
+
+    /**
+     * AJAX request POST key that acts as a namespace for request handling
+     *
+     * @var string
+     */
+    private $ajax_namespace;
+
+    /**
+     * POST request POST key that acts as a namespace for request handling
+     *
+     * @var string
+     */
+    private $post_namespace;
+
+    /**
+     * WordPress nonce key for POST/AJAX requests
+     *
+     * @var string
+     */
+    private $nonce_key;
+
+    /**
+     * WordPress nonce name for POST/AJAX requests
+     *
+     * @var string
+     */
+    private $nonce_name;
+
+    /**
+     * WordPress nonce token
+     *
+     * @var string
+     */
+    private $nonce;
+
+    /**
+     * WordPress nonce html field for forms
+     *
+     * @var string
+     */
+    private $nonce_field;
+
+    /**
+     * Defines namespaces for requests. Defines nonce data
+     *
+     * @param string $ajax_namespace
+     * @param string $post_namespace
+     * @param string $nonce_key
+     * @param string $nonce_name
+     */
+    public function __construct(
+        $ajax_namespace,
+        $post_namespace,
+        $nonce_key,
+        $nonce_name
+    ) {
+        $this->ajax_namespace = $ajax_namespace;
+        $this->post_namespace = $post_namespace;
+        $this->nonce_key = $nonce_key;
+        $this->nonce_name = $nonce_name;
+        $this->nonce = wp_create_nonce($this->nonce_name);
+        $this->nonce_field = wp_nonce_field($this->nonce_name, $this->nonce_key, true, false);
+    }
+
+    /**
+     * Delegates requests to the appropriate class handler method
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @param string     $route
+     * @param string     $method or function
+     * @param mixed|null $model
+     * @param function[] $middlewares array of middleware functions
+     * @return void
+     */
+    public function add($route, $method, $model = null, $middlewares = []) {
+        if (isset($_POST[$this->ajax_namespace]) && !empty($_POST[$this->ajax_namespace])) {
+            if (isset($_POST['route'])) {
+                if ($_POST['route'] === $route) {
+                    check_ajax_referer($this->nonce_name, $this->nonce_key);
+                    $payload = $this->getJsonPayload();
+                    foreach ($middlewares as $middleware) {
+                        $middleware($payload);
+                    }
+                    $this->processAJAX($method, $model, $payload);
+                }
+            }
+        }
+
+        if (isset($_POST[$this->post_namespace]) && !empty($_POST[$this->post_namespace])) {
+            if (isset($_POST['route'])) {
+                if ($_POST['route'] === $route) {
+                    $nonce = '';
+                    if (isset($_REQUEST[$this->nonce_key])) {
+                        $nonce = sanitize_text_field(wp_unslash($_REQUEST[$this->nonce_key]));
+                    }
+                    if (!wp_verify_nonce($nonce, $this->nonce_name)) {
+                        die('Security check');
+                    }
+                    $payload = $this->getJsonPayload();
+                    foreach ($middlewares as $middleware) {
+                        $middleware($payload);
+                    }
+                    $this->processPOST($method, $model, $payload);
+                }
+            }
+        }
+    }
+
+    /**
+     * Decodes JSON body if set. Returns null if not set
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @return array|null
+     */
+    public function getJsonPayload() {
+        $body = isset($_POST['payload']) ? $_POST['payload'] : null;
+        if (empty($body)) {
+            $body = null;
+        }
+        $payload = json_decode($body, true);
+        if ($payload === null) {
+            $payload = json_decode(wp_unslash($body), true);
+        }
+
+        return $payload;
+    }
+
+    /**
+     * Creates a POST form with a given route and inner html if provided
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @param string $route
+     * @param string $innerhtml
+     * @return string
+     */
+    public function createPostForm($route, $innerhtml = '') {
+        ob_start();
+        ?>
+        <form action="<?php echo site_url() ?>" method="post">
+            <?php echo $this->nonce_field; ?>
+            <input type="hidden" name="<?php echo $this->post_namespace ?>" value="1" />
+            <input type="hidden" name="route" value="<?php echo $route ?>" />
+            <?php echo $innerhtml ?>
+        </form>
+        <?php
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        return $html;
+    }
+
+    /**
+     * Script injection variables to be used for AJAX requests
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @return array
+     */
+    public function getScriptInjectionVariables() {
+        return array(
+            'nonceKey' => $this->nonce_key,
+            'nonce' => $this->nonce,
+            'nonceField' => $this->nonce_field,
+            'ajaxNamespace' => $this->ajax_namespace,
+            'postNamespace' => $this->post_namespace,
+        );
+    }
+}
