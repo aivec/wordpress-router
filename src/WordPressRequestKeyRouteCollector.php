75c02d46931964d7d504e0e6012219fbb729ef9a
Evan Shaw <evandanielshaw@gmail.com>
2021-04-28 20:38:43 +0900

    fix: fix method declaration of WordPressRequestKeyRouteCollector's addWordPressRoute and add support for multiple roles

--- src/WordPressRequestKeyRouteCollector.php
+++ src/WordPressRequestKeyRouteCollector.php
@@ -22,7 +22,7 @@ class WordPressRequestKeyRouteCollector extends WordPressRouteCollector
      * @param callable[]      $aftermiddlewares array of callables to be invoked after
      *                                          the route callable returns
      * @param boolean         $noncecheck
-     * @param string          $role role to check for the current user
+     * @param string[]|null   $roles roles to check for the current user
      * @return void
      */
     public function addWordPressRoute(
@@ -32,14 +32,14 @@ class WordPressRequestKeyRouteCollector extends WordPressRouteCollector
         array $middlewares = [],
         array $aftermiddlewares = [],
         $noncecheck = false,
-        $role = ''
+        array $roles = null
     ) {
         $this->addRoute($method, $route, function ($args) use (
             $middlewares,
             $aftermiddlewares,
             $callable,
             $noncecheck,
-            $role
+            $roles
         ) {
             if ($noncecheck === true) {
                 $nonce = '';
@@ -50,10 +50,17 @@ class WordPressRequestKeyRouteCollector extends WordPressRouteCollector
                     die('Forbidden');
                 }
             }
-            if (!empty($role)) {
+            if (!empty($roles)) {
+                $exists = false;
                 $user = wp_get_current_user();
-                if (!in_array(strtolower($role), (array)$user->roles, true)) {
-                    http_response_code(403);
+                foreach ($roles as $role) {
+                    if (in_array(strtolower($role), (array)$user->roles, true)) {
+                        $exists = true;
+                        break;
+                    }
+                }
+
+                if ($exists === false) {
                     die('Forbidden');
                 }
             }
